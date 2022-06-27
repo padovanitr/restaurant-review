91d04bed65c4de088d8ec1c2aab4498ab9b24ab1
Thiago Padovani <padovanitr@gmail.com>
2022-06-27 15:49:15 +0100

    set up

---
+++ src/App.style.ts
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material'
+import { styled } from '@mui/material/styles'
+
+export const AppContainer = styled(Box)`
+  height: 100vh;
+  width: 100vw;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+`
