3987d1e9e5153ad91be4c50d4917b8d66cca1821
Thiago Padovani <padovanitr@gmail.com>
2022-06-27 17:48:01 +0100

    render google map

--- src/ui/pages/Home.tsx
+++ src/ui/pages/Home.tsx
@@ -1,5 +1,29 @@
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api'
+import { useCallback } from 'react'
+
+const containerStyle = {
+  width: '500px',
+  height: '500px',
+}
+
+const center = {
+  lat: -3.745,
+  lng: -38.523,
+}
+
 function Home() {
-  return <h1>Home</h1>
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: 'AIzaSyD8oKLh-p7_hSxhSg03u_nRkN2RPYpl720',
+  })
+
+  const onLoad = useCallback((map: unknown) => {
+    console.log('map', map)
+  }, [])
+
+  const onUnmount = useCallback(() => {}, [])
+
+  return isLoaded && <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={12} />
 }
 
 export default Home
