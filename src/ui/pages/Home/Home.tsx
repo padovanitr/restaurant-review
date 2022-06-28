488845be8febea1d208d33be986c911b6e76b5d8
Thiago Padovani <padovanitr@gmail.com>
2022-06-28 12:15:00 +0100

    fetch restaurants data from google places api

--- src/ui/pages/Home/Home.tsx
+++ src/ui/pages/Home/Home.tsx
@@ -7,23 +7,50 @@ const containerStyle = {
   height: '500px',
 }
 
-interface UserCoords {
+interface UserCoordsType {
   lat: number
   lng: number
 }
 
 function Home() {
-  const [userCoords, setUserCoords] = useState<UserCoords>()
+  const [userCoords, setUserCoords] = useState<UserCoordsType>()
+  const [googleRestaurants, setGoogleRestaurants] = useState<unknown>([])
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: 'AIzaSyD8oKLh-p7_hSxhSg03u_nRkN2RPYpl720',
   })
 
+  const handleMarkerClick = (event: google.maps.MapMouseEvent) => {
+    console.log('event', event)
+  }
+
+  const fetchGooglePlaces = (map: HTMLDivElement | google.maps.Map) => {
+    if (!userCoords) {
+      return
+    }
+    const pyrmont = new window.google.maps.LatLng(userCoords.lat, userCoords.lng)
+
+    const request = {
+      location: pyrmont,
+      radius: 1000,
+      type: 'restaurant',
+    }
+
+    const service = new google.maps.places.PlacesService(map)
+
+    service.nearbySearch(request, (results, status) => {
+      if (status === 'OK') {
+        console.log('results', results)
+        setGoogleRestaurants(results)
+      }
+    })
+  }
+
   const getUserLocation = async () => {
     const { coords } = await new Promise((res, rej) => {
       navigator.geolocation.getCurrentPosition(res, rej)
     })
-    console.log('coordinates', coords)
+
     const userLocation = {
       lat: coords.latitude,
       lng: coords.longitude,
@@ -38,8 +65,19 @@ function Home() {
   return (
     <Box>
       {isLoaded && userCoords && (
-        <GoogleMap mapContainerStyle={containerStyle} center={userCoords} zoom={11}>
-          {userCoords && <Marker position={userCoords} />}
+        <GoogleMap
+          onLoad={(map) => fetchGooglePlaces(map)}
+          mapContainerStyle={containerStyle}
+          center={userCoords}
+          zoom={11}
+        >
+          {userCoords && (
+            <Marker
+              title="My position"
+              position={userCoords}
+              onClick={(e) => handleMarkerClick(e)}
+            />
+          )}
         </GoogleMap>
       )}
     </Box>
