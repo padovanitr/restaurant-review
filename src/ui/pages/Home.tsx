5ebad787e0c0012048a93d8285f0c20c996db8bd
Thiago Padovani <padovanitr@gmail.com>
2022-06-28 09:58:41 +0100

    get and set user position with marker, updates favicon

--- src/ui/pages/Home.tsx
+++ src/ui/pages/Home.tsx
@@ -1,8 +1,8 @@
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api'
-import { useCallback } from 'react'
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api'
+import { useEffect, useState } from 'react'
 
 const containerStyle = {
-  width: '500px',
+  width: '600px',
   height: '500px',
 }
 
@@ -11,19 +11,42 @@ const center = {
   lng: -38.523,
 }
 
+interface UserCoords {
+  lat: number
+  lng: number
+}
+
 function Home() {
+  const [userCoords, setUserCoords] = useState<UserCoords>()
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: 'AIzaSyD8oKLh-p7_hSxhSg03u_nRkN2RPYpl720',
   })
 
-  const onLoad = useCallback((map: unknown) => {
-    console.log('map', map)
+  const getUserLocation = async () => {
+    const { coords } = await new Promise((res, rej) => {
+      navigator.geolocation.getCurrentPosition(res, rej)
+    })
+    console.log('coordinates', coords)
+    const userLocation = {
+      lat: coords.latitude,
+      lng: coords.longitude,
+    }
+    setUserCoords(userLocation)
+  }
+
+  useEffect(() => {
+    getUserLocation()
   }, [])
 
-  const onUnmount = useCallback(() => {}, [])
-
-  return isLoaded && <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={12} />
+  return (
+    isLoaded &&
+    userCoords && (
+      <GoogleMap mapContainerStyle={containerStyle} center={userCoords} zoom={11}>
+        {userCoords && <Marker position={userCoords} />}
+      </GoogleMap>
+    )
+  )
 }
 
 export default Home
