e86acbc2eab7daa7a13fe0fc4c57715d62881844
Thiago Padovani <padovanitr@gmail.com>
2022-06-28 12:57:39 +0100

    fix: places result typing

--- src/ui/pages/Home/Home.tsx
+++ src/ui/pages/Home/Home.tsx
@@ -1,6 +1,6 @@
 import { Box } from '@mui/material'
 import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api'
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 const containerStyle = {
   width: '600px',
@@ -12,39 +12,50 @@ interface UserCoordsType {
   lng: number
 }
 
+interface GoogleRestaurantsType {
+  place_id: string
+  geometry: {
+    location: {
+      lat: () => number
+      lng: () => number
+    }
+  }
+}
+
 function Home() {
   const [userCoords, setUserCoords] = useState<UserCoordsType>()
-  const [googleRestaurants, setGoogleRestaurants] = useState<unknown>([])
+  const [googleRestaurants, setGoogleRestaurants] = useState<GoogleRestaurantsType[]>([])
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: 'AIzaSyD8oKLh-p7_hSxhSg03u_nRkN2RPYpl720',
   })
 
-  const handleMarkerClick = (event: google.maps.MapMouseEvent) => {
-    console.log('event', event)
-  }
+  const handleMarkerClick = (event: google.maps.MapMouseEvent) => {}
 
-  const fetchGooglePlaces = (map: HTMLDivElement | google.maps.Map) => {
-    if (!userCoords) {
-      return
-    }
-    const pyrmont = new window.google.maps.LatLng(userCoords.lat, userCoords.lng)
-
-    const request = {
-      location: pyrmont,
-      radius: 1000,
-      type: 'restaurant',
-    }
-
-    const service = new google.maps.places.PlacesService(map)
-
-    service.nearbySearch(request, (results, status) => {
-      if (status === 'OK') {
-        console.log('results', results)
-        setGoogleRestaurants(results)
+  const fetchGooglePlaces = useCallback(
+    (map: HTMLDivElement | google.maps.Map) => {
+      if (!userCoords) {
+        return
       }
-    })
-  }
+      const pyrmont = new window.google.maps.LatLng(userCoords.lat, userCoords.lng)
+
+      const request = {
+        location: pyrmont,
+        radius: 1500,
+        type: 'restaurant',
+      }
+
+      const service = new google.maps.places.PlacesService(map)
+
+      service.nearbySearch(request, (results, status) => {
+        if (status === 'OK') {
+          console.log('results', results)
+          setGoogleRestaurants(results as GoogleRestaurantsType[])
+        }
+      })
+    },
+    [userCoords]
+  )
 
   const getUserLocation = async () => {
     const { coords } = await new Promise((res, rej) => {
@@ -69,7 +80,7 @@ function Home() {
           onLoad={(map) => fetchGooglePlaces(map)}
           mapContainerStyle={containerStyle}
           center={userCoords}
-          zoom={11}
+          zoom={13}
         >
           {userCoords && (
             <Marker
@@ -78,6 +89,18 @@ function Home() {
               onClick={(e) => handleMarkerClick(e)}
             />
           )}
+
+          {googleRestaurants &&
+            googleRestaurants.map((restaurant) => (
+              <Marker
+                key={restaurant.place_id}
+                title="My position"
+                position={{
+                  lat: restaurant.geometry.location.lat(),
+                  lng: restaurant.geometry.location.lng(),
+                }}
+              />
+            ))}
         </GoogleMap>
       )}
     </Box>
