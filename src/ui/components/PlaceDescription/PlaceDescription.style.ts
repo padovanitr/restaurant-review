6ac7bd02c456b357f8198373e7619b9e2ab14976
Thiago Padovani <padovanitr@gmail.com>
2022-09-17 11:34:12 -0300

    remove lint error

--- src/ui/components/PlaceDescription/PlaceDescription.style.ts
+++ src/ui/components/PlaceDescription/PlaceDescription.style.ts
@@ -2,8 +2,8 @@ import { styled, css, Box } from '@mui/material'
 
 export const PlaceDescriptionWrapper = styled(Box)(
   ({ theme }) => css`
-    display: flex
-    align-items: center
-    justify-content: center
+    display: flex;
+    align-items: center;
+    justify-content: center;
   `
 )
